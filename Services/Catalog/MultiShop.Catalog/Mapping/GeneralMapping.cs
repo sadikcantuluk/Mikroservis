acb4d66780aa8365ce1309e72b0c6381dcb1eaaa
Sadikcan Tuluk <sadikcantuluk@gmail.com>
2024-06-26 09:27:54 +0300

    Catalog mikroservisi oluşturuldu ve MongoDb bağlantısı kuruldu.

---
+++ Services/Catalog/MultiShop.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.Catalog.Dtos.ProductDetailDtos;
+using MultiShop.Catalog.Dtos.ProductDtos;
+using MultiShop.Catalog.Dtos.ProductImageDtos;
+using MultiShop.Catalog.Entities;
+
+namespace MultiShop.Catalog.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<ResultCategoryDto, Category>().ReverseMap();
+            CreateMap<UpdateCategoryDto, Category>().ReverseMap();
+            CreateMap<CreateCategoryDto, Category>().ReverseMap();
+            CreateMap<GetByIdCategoryDto, Category>().ReverseMap();
+
+            CreateMap<ResultProductDto, Product>().ReverseMap();
+            CreateMap<CreateProductDto, Product>().ReverseMap();
+            CreateMap<UpdateProductDto, Product>().ReverseMap();
+            CreateMap<GetByIdProductDto, Product>().ReverseMap();
+
+            CreateMap<ResultProductDetailDto, Product>().ReverseMap();
+            CreateMap<UpdateProductDetailDto, Product>().ReverseMap();
+            CreateMap<CreateProductDetailDto, Product>().ReverseMap();
+            CreateMap<GetByIdProductDetailDto, Product>().ReverseMap();
+
+            CreateMap<ResultProductImageDto, Product>().ReverseMap();
+            CreateMap<UpdateProductImageDto, Product>().ReverseMap();
+            CreateMap<CreateProductImageDto, Product>().ReverseMap();
+            CreateMap<GetByIdProductImageDto, Product>().ReverseMap();
+        }
+    }
+}
