b6a4c2f1e92ef030dc819c648b0e0b4fbd66d085
Sadikcan Tuluk <sadikcantuluk@gmail.com>
2024-06-27 22:32:28 +0300

    Dapper kullanılarak Discount mikroservisi oluşturuldu ve services ile api yapıları yazıldı.

---
+++ Services/Discount/MultiShop.Discount/Services/DiscountCouponService.cs
@@ -0,0 +1,79 @@
+﻿using Dapper;
+using MultiShop.Discount.Context;
+using MultiShop.Discount.Dtos;
+
+namespace MultiShop.Discount.Services
+{
+    public class DiscountCouponService : IDiscountCouponService
+    {
+        private readonly DapperContext _dapperContext;
+
+        public DiscountCouponService(DapperContext dapperContext)
+        {
+            _dapperContext = dapperContext;
+        }
+
+        public async Task CreateDiscountCouponAsync(CreateDiscountCouponDto createCouponDto)
+        {
+            var query = "insert into coupons (Code,Rate,IsActive,ValidDate) values (@code,@rate,@isActive,@validDate)";
+            var parameters = new DynamicParameters();
+            parameters.Add("@code", createCouponDto.Code);
+            parameters.Add("@rate", createCouponDto.Rate);
+            parameters.Add("@isActive", createCouponDto.IsActive);
+            parameters.Add("@validDate", createCouponDto.ValidDate);
+            using (var connection = _dapperContext.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+
+        }
+
+        public async Task DeleteDiscountCouponAsync(int id)
+        {
+            var query = "delete from coupons where CouponId = @id";
+            var parameters = new DynamicParameters();
+            parameters.Add("@id", id);
+            using (var connection = _dapperContext.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async Task<List<ResultDiscountCouponDto>> GetAllDiscountCouponsAsync()
+        {
+            var query = "select * from coupons";
+            using (var connection = _dapperContext.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultDiscountCouponDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public async Task<GetByIdDiscountCouponDto> GetByIdDiscountCouponAsync(int id)
+        {
+            var query = "select * from coupons where CouponId = @id";
+            var parameters = new DynamicParameters();
+            parameters.Add("@id", id);
+            using (var connection = _dapperContext.CreateConnection())
+            {
+                var value = await connection.QueryFirstOrDefaultAsync<GetByIdDiscountCouponDto>(query,parameters);
+                return value;
+            }
+        }
+
+        public async Task UpdateDiscountCouponAsync(UpdateDiscountCouponDto updateCouponDto)
+        {
+            var query = "update coupons set Code = @code,Rate = @rate,IsActive = @isActive,ValidDate = @validDate where CouponId = @couponId";
+            var parameters = new DynamicParameters();
+            parameters.Add("@code", updateCouponDto.Code);
+            parameters.Add("@rate", updateCouponDto.Rate);
+            parameters.Add("@isActive", updateCouponDto.IsActive);
+            parameters.Add("@validDate", updateCouponDto.ValidDate);
+            parameters.Add("@couponId", updateCouponDto.CouponId);
+            using (var connection = _dapperContext.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+    }
+}
---
+++ Services/Discount/MultiShop.Discount/Services/IDiscountCouponService.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.Discount.Dtos;
+
+namespace MultiShop.Discount.Services
+{
+    public interface IDiscountCouponService
+    {
+        Task<List<ResultDiscountCouponDto>> GetAllDiscountCouponsAsync();
+        Task<GetByIdDiscountCouponDto> GetByIdDiscountCouponAsync(int id);
+        Task UpdateDiscountCouponAsync(UpdateDiscountCouponDto updateCouponDto);
+        Task DeleteDiscountCouponAsync(int id);
+        Task CreateDiscountCouponAsync(CreateDiscountCouponDto createCouponDto);
+    }
+}
